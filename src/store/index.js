7eb5d55268a7ee569a6414eb3f44f7e76260120c
awhendr261101 <awodwasihlehendrick@gmail.com>
2025-02-04 08:41:30 +0200

    Done Animation

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    about : null,
+    skills: null,
+    experience: null,
+    education: null,
+    projects: null
+  },
+
+  mutations: {
+
+  },
+  actions: {
+    
+  }
+
+})
