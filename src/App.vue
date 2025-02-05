0a1bf854ad7c1ee0172a239ab73d1824445acb76
awhendr261101 <awodwasihlehendrick@gmail.com>
2025-02-05 16:32:48 +0200

    Final

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,6 @@ import Aos from 'aos';
 import 'aos/dist/aos.css';
 import NavBar from './components/NavBar.vue';
 import { ref, onMounted } from 'vue';
-import axios from 'axios';
 
 const visitorInfo = ref(null);
 console.log(visitorInfo);
@@ -23,32 +22,9 @@ console.log(visitorInfo);
 
 onMounted(() =>{
   Aos.init(); 
-}),
-
-onMounted(async () => {
-    try {
-        const { data } = await axios.get("https://api64.ipify.org?format=json");
-        const ip = data.ip;
-        const ipDetails = await axios.get(`https://ipinfo.io/${ip}/json`);
-        const visitorData = {
-            ip: ip,
-            city: ipDetails.data.city,
-            isp: ipDetails.data.isp,
-            browser: navigator.userAgent,
-            screenWidth: window.screen.width,
-            screenHeight: window.screen.height,
-            referrer: document.referrer || "Direct Visit",
-            visitTime: new Date().toISOString(),
-        };
-
-        // const response = await axios.post("https://localhost:3050/addviewer", visitorData);
-        
-        // visitorInfo.value = response.data;
-        console.log("Visitor logged:", visitorData);
-    } catch (error) {
-        console.error("Error logging visitor:", error);
-    }
 })
+
+
 
 
 </script>
